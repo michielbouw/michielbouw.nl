861499d922037e4d1cef9df4104d2c1f5ad39d5c
M P Bouw <superchiel94@hotmail.com>
2019-07-05 14:34:47 +0200

    add background

--- build.sh
+++ build.sh
@@ -5,7 +5,10 @@ rm -rf dist
 mkdir dist
 
 # html min
-html-minifier --collapse-whitespace --remove-comments --remove-optional-tags --remove-redundant-attributes --remove-script-type-attributes --remove-tag-whitespace --minify-css --use-short-doctype index.html -o dist/index.html
+html-minifier --collapse-whitespace --remove-comments --remove-optional-tags --remove-redundant-attributes --remove-script-type-attributes --remove-tag-whitespace --use-short-doctype index.html -o dist/index.html
+
+# css min
+uglifycss base.css > dist/base.css
 
 # move static
 mkdir dist/assets
